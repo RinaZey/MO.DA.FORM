409395013e74165dd59cdd653e08be70d3450233
H1kkar1 <kn_pavel@mail.ru>
2023-10-18 13:34:50 +0300

    Добавьте файлы проекта.

---
+++ Mo.da.form/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MO.DA.FORM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
